10514071be33e0e4ff8f18e750c9b2d769dcf461
Praise Paul <praise20024christ@gmail.com>
2026-09-10 21:56:03 +0530

    feat(cms): load booking services from MongoDB

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,15 +1,11 @@
 import { BookingFlow } from "@/components/public/booking/booking-flow";
 import { PublicFooter } from "@/components/public/layout/public-footer";
 import { PublicHeader } from "@/components/public/navigation/public-header";
+import { getServices } from "@/lib/cms/services-repository";
 
-export default function BookPage() {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <PublicHeader />
-      <main className="flex-1">
-        <BookingFlow />
-      </main>
-      <PublicFooter />
-    </div>
-  );
+export const dynamic = "force-dynamic";
+
+export default async function BookPage() {
+  const services = await getServices();
+  return <div className="flex min-h-screen flex-col"><PublicHeader /><main className="flex-1"><BookingFlow services={services} /></main><PublicFooter /></div>;
 }
