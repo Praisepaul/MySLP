8622e028a16a3783ac67873fedad25fba14d83af
Praise Paul <praise20024christ@gmail.com>
2026-09-10 16:08:17 +0530

    Add Google Calendar connection types

---
+++ lib/calendar/google-calendar-types.ts
@@ -0,0 +1,27 @@
+import type { ObjectId } from "mongodb";
+
+export const googleCalendarConnectionId = "therapist";
+
+export type GoogleCalendarConnectionDocument = {
+  _id: typeof googleCalendarConnectionId;
+  provider: "google";
+  calendarId: string;
+  encryptedRefreshToken: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type GoogleCalendarConnectionStatus = {
+  connected: boolean;
+  calendarId?: string;
+  connectedAt?: string;
+};
+
+export type GoogleCalendarBusyInterval = {
+  start: Date;
+  end: Date;
+};
+
+export type GoogleCalendarConnectionMongoDocument = GoogleCalendarConnectionDocument & {
+  _id: typeof googleCalendarConnectionId | ObjectId;
+};
