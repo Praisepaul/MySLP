31812ad2378ef78c6af5fef36a0726a361c3fb13
Praise Paul <praise20024christ@gmail.com>
2026-09-11 15:45:51 +0530

    Protect the complete admin route tree

---
+++ app/admin/layout.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+import { isAdminAuthenticated } from "@/lib/admin/auth";
+
+export const dynamic = "force-dynamic";
+
+export default async function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  if (!(await isAdminAuthenticated())) redirect("/admin-login");
+  return children;
+}
